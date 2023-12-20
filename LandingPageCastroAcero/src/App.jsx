14be542b09a794c0bdb736d6a5db771f8502cadb
PABLO TALONIA <94588789+Pablolsd25@users.noreply.github.com>
2023-12-20 06:11:17 +0000

    Landing finalizada//Subir a producción

--- LandingPageCastroAcero/src/App.jsx
+++ LandingPageCastroAcero/src/App.jsx
@@ -8,21 +8,17 @@ function App() {
     <>
       <div className="container-fluid d-flex align-items-center justify-content-center vh-100 mt-20 text-center">
         <div>
-          <Image
-            src={Logo}
-            alt="Logo"
-            roundedCircle
-            className="Logo-img img-fluid"
-          />
-          <p>
-            Grupo Castro Acero con mas de 50 años de experiencia en la
-            distribucion de materiales para la construcción. Contactanos y pide
-            tu cotización, respuesta inmediata
+          <Image src={Logo} alt="Logo" rounded className="Logo-img img-fluid" />
+          <p className="text-white text-break">
+            Grupo Castro Acero, con más de 50 años de excelencia en la
+            distribución de materiales para la construcción, se pone a tu
+            disposición. Contáctanos ahora para solicitar tu cotización y
+            recibir una respuesta inmediata. Confía en nuestra experiencia para
+            satisfacer tus necesidades en el ámbito de la construcción.
           </p>
           <Contendor />
         </div>
       </div>
-      
     </>
   );
 }
--- LandingPageCastroAcero/src/Components/Contenedor.jsx
+++ LandingPageCastroAcero/src/Components/Contenedor.jsx
@@ -1,22 +1,115 @@
 import ListGroup from "react-bootstrap/ListGroup";
-
+import Facebook from "../assets/facebook.svg";
+import WhatsApp from "../assets/whatsapp.svg";
+import Instagram from "../assets/instagram.svg";
+import Tiktok from "../assets/tik-tok.svg";
 export default function Contenedor() {
   return (
     <ListGroup>
       <ListGroup.Item action variant="danger">
-        Página Web
+        <a
+          href="https://www.castroacero.com.mx"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-danger"
+        >
+          Página Web
+        </a>
       </ListGroup.Item>
       <ListGroup.Item action variant="danger">
-        Catálogo PDF
+        <a
+          href="https://drive.google.com/file/d/1jThYugirC9L1zvkYFq0kIUCp8dOBaZR-/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-danger"
+        >
+          Catálogo PDF
+        </a>
       </ListGroup.Item>
       <ListGroup.Item action variant="danger">
-        Video Presentación
+        <a
+          href="https://www.youtube.com/watch?v=EJQK_ZLbWeE"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-danger"
+        >
+          Video de presentación
+        </a>
       </ListGroup.Item>
       <ListGroup.Item action variant="danger">
-        Sucursal Peralvillo
+        <a
+          href="https://maps.app.goo.gl/jHTNuYhv8dde8Mc37"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-danger"
+        >
+          Sucursal Peralvillo, CDMX
+        </a>
       </ListGroup.Item>
       <ListGroup.Item action variant="danger">
-        Sucursal Tecámac
+        <a
+          href="https://maps.app.goo.gl/hN4qLydPpw2dfePm6"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-danger"
+        >
+          Sucursal Tecámac, Edo. de México
+        </a>
+      </ListGroup.Item>
+      <ListGroup.Item action variant="danger">
+        <h1>Redes sociales</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-3">
+              {/* Icono de Facebook */}
+              <a
+                href="https://www.facebook.com/grupocastroacero.mx/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white"
+              >
+                <img src={Facebook} alt="Facebook" className="h2" />
+                <i className="bi bi-facebook h2"></i>
+              </a>
+            </div>
+            <div className="col-3">
+              {/* Icono de WhatsApp */}
+              <a
+                href="https://api.whatsapp.com/send?phone=525517705007"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white"
+              >
+                <img src={WhatsApp} alt="WhatsApp" className="h2" />
+                <i className="bi bi-whatsapp h2"></i>
+              </a>
+            </div>
+            <div className="col-3">
+              {/* Icono de Instagram */}
+              <a
+                href="https://www.instagram.com/castroacero.mx/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white"
+              >
+                <img src={Instagram} alt="Instagram" className="h2" />
+                <i className="bi bi-instagram h2"></i>
+              </a>
+            </div>
+            <div className="col-3">
+              {/* Icono de TikTok */}
+              <a
+                href="https://www.tiktok.com/@grupocastroacero.mx"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white"
+              >
+                <img src={Tiktok} alt="Tiktok" className="h2" />
+                <i className="bi bi-tiktok h2"></i>
+              </a>
+            </div>
+          </div>
+        </div>
       </ListGroup.Item>
     </ListGroup>
   );
